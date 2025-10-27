ad27b59093c222e504278c882337059737739fbe
GeronimoAhil <geromy2014@gmail.com>
2025-10-27 16:21:18 +0800

    Add files via upload

---
+++ acp.py
@@ -0,0 +1,11 @@
+ahil="12 october"
+brother="7 june"
+bob="26 July"
+darcy="9 December"
+lily="26 November"
+
+print("Ahil's Birthday is on",ahil) 
+print("Brother's Birthday is on",brother)
+print("Bob's Birthday is on",bob)
+print("Darcy's Birthday is on",darcy)
+print("Lily's Birthday is on",lily)
